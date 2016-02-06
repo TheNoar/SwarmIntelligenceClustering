386f9eecfa9d63ce3f014ff276952a050f2b5acc
Thenoar <nodi2451@colorado.edu>
2016-02-05 17:04:50 -0700

    added data file

--- clustering.py
+++ clustering.py
@@ -2,4 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
-print("hi")
+class test:
+	summatrix = []
+	count =0
+	listmatix = []
+	
+
+
+
+f = open("semeion.data")
+count = 0
+for line in f:
+	name = 'test'+str(count)+'.png'
+	data = line.rstrip('\r\n').split(' ')
+	data = map(float,data[0:256])
+	plt.imsave(name,np.array(data).reshape(16,16), cmap=cm.gray)
+	count+=1
