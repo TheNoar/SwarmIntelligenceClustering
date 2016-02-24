919e9eba4f929d85c6d3599b7fa481dc417a2779
TheNoar <nodi2451@colorado.edu>
2016-02-23 18:23:56 -0700

    updated stuff

--- clustering.py
+++ clustering.py
@@ -1,11 +1,11 @@
 import numpy as np
 import random as rand
-# import matplotlib.pyplot as plt
-# import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 # constants?
-kd = 0.5
-kp = 0.5
+kd = 0.1
+kp = 0.6
 
 threshold = 20
 
@@ -30,25 +30,44 @@ class pile:
 		return v
 
 
-def f(pileV, xV):
-	pnorm = np.linalg.norm(pileV)
-	xnorm = np.linalg.norm(pileV)
+def f1(pile, xV):
+	pnorm = np.linalg.norm(pile.sumvecter)
+	xnorm = np.linalg.norm(pile.sumvecter)
 	
 	if((xnorm == 0.0) or (pnorm == 0.0)):
 		return 0.0
 
-	v1 = pileV/pnorm
+	v1 = pile.sumvecter/pnorm
 	v2 = xV/xnorm
 
 	dot = np.dot(v1,v2)
 	
 	return abs(dot)
 
-def drop(pile, vector):
-	fx = f(pile.sumvecter, vector)
+def f2(pile,xV):
+	Va = pile.sumvecter/pile.count
+
+	dot = np.dot(Va,xV)
+	
+	return abs(dot)/sum(xV)
+
+def f3(pile,xV):
+	if pile.count == 0:
+		return 1
+	difsum = np.linalg.norm(xV - pile.sumvecter/pile.count)
+
+	# for v in pile.vecterlist:
+	# 	dif = abs(v - xV)
+	# 	difsum += sum(dif)
+	# avrdif = difsum/pile.count
+
+	return difsum
+
+def drop(pile, vector, func):
+	fx = func(pile, vector)
 	choice = rand.random()
-	if fx < kd:
-		if 2*fx > choice:
+	if fx > kd:
+		if 2*fx < choice:
 			pile.add(vector)
 			return True
 		else:
@@ -58,14 +77,14 @@ def drop(pile, vector):
 		return True
 
 
-def pickup(pile, index):
+def pickup(pile, index, func):
 	vec = pile.vecterlist[index]
-	fx = f(pile.sumvecter - vec, vec)
+	fx = func(pile, vec)
 	choice = rand.random()
 
 	prob = (kp/(kp+fx))**2
 
-	if choice < prob:
+	if choice > prob:
 		vec = pile.remove(index)
 		return vec, True
 	else:
@@ -74,47 +93,77 @@ def pickup(pile, index):
 
 def main():
 	f = open("semeion.data")
-	count = 0
 
-	piles = []
+	func = f3
+
+	count = 0
+    # piles = [pile() for i in xrange(10)]
+	piles = [pile(),pile(),pile(),pile(),pile(),pile(),pile(),pile(),pile(),pile()]
 
 	for line in f:
 		data = line.rstrip('\r\n').split(' ')
 		data = map(float,data[0:256])
 		vec = np.array(data)
-		p = pile()
-		p.add(vec)
-		piles.append(p)
+		piles[count%10].add(vec)
+		# p = pile()
+		# p.add(vec)
 		count+=1
 
 	# for testing
 	# threshold = count/2
 
+	# print count
+
 	running = True
 	holding = False
 	hands = 0
-	while(running):
-		indexp = rand.randint(0,count-1)
-
+	# while(running):
+	print "here"
+	for q in xrange(500000):
+		indexp = rand.randint(0,9)
+		# print q
 		if(holding):
-			done = drop(piles[indexp],hands)
+			done = drop(piles[indexp],hands, func)
 			if(done):
 				holding = False
 
 		else:
-			indexv = rand.randint(0,piles[indexp].count)
+			indexv = 0
+			if(piles[indexp].count>1):
+				indexv = rand.randint(0,piles[indexp].count-1)
+			elif(piles[indexp].count == 0):
+				continue
+			hands, holding = pickup(piles[indexp],indexv,func)
 
-			hands, done = pickup(piles[indexp],indexv)
+			# if(done):
+			# 	holding = True
+				# if(piles[indexp].count == 0):
+				# 	piles.pop(indexp)
+				# 	count -= 1
 
-			if(done):
-				holding = True
-				if(piles[indexp].count == 0):
-					piles.pop(indexp)
-					count -= 1
+		# if(count < threshold):
+		# 	running = False
 
-		if(count < threshold):
-			running = False
+	print "almost done"
 
-	
+	for p in piles:
+		avr = 0
+		if p.count == 0:
+			continue
+		for v in p.vecterlist:
+			avr += func(p,v)
+		print(avr/p.count)
+
+	c = 0
+	for p in piles:
+		if p.count == 0:
+			continue
+		name = 'test'+str(c)+'.png'
+		# data = map(lambda x: 1 if x>0.4 else 0, p.sumvecter/p.count)
+		data = p.sumvecter/p.count
+		plt.imsave(name,np.array(data).reshape(16,16),cmap=cm.gray)
+		c+=1
+
+
 
 main()
